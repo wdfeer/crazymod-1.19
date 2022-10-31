02585054f697a7c6f745c4b2160e017a4881693f
wdfeer <wdf-is-git@tutanota.de>
2022-10-31 21:05:32 +0100

    Add TitaniumOre

--- src/main/java/net/wdfeer/crazymod/item/ModItems.java
+++ src/main/java/net/wdfeer/crazymod/item/ModItems.java
@@ -11,12 +11,14 @@ import net.wdfeer.crazymod.CrazyMod;
 import net.wdfeer.crazymod.toolmaterial.TitaniumMaterial;
 
 public class ModItems {
+    public static final Item TITANIUM_ORE = RegisterItem("titanium_ore",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof()));
     public static final Item TITANIUM_INGOT = RegisterItem("titanium_ingot",
             new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).fireproof()));
     public static final Item TITANIUM_SWORD = RegisterItem("titanium_sword",
             new SwordItem(TitaniumMaterial.INSTANCE, 9, -2.4f, new FabricItemSettings().group(ItemGroup.COMBAT).fireproof()));
     public static final Item TITANIUM_PICKAXE = RegisterItem("titanium_pickaxe",
-            new PickaxeItem(TitaniumMaterial.INSTANCE, 5, -3f, new FabricItemSettings().group(ItemGroup.TOOLS).fireproof()));
+            new PickaxeItem(TitaniumMaterial.INSTANCE, 5, -2.5f, new FabricItemSettings().group(ItemGroup.TOOLS).fireproof()));
     static Item RegisterItem(String name, Item item)
     {
         return Registry.register(Registry.ITEM, new Identifier(CrazyMod.MOD_ID, name), item);
