7de58aba1620a19f7ca36f77c60bbe0345887195
wdfeer <wdf-is-git@tutanota.de>
2022-10-31 18:06:38 +0100

    Add TitaniumSword

--- src/main/java/net/wdfeer/crazymod/item/ModItems.java
+++ src/main/java/net/wdfeer/crazymod/item/ModItems.java
@@ -6,10 +6,12 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.wdfeer.crazymod.CrazyMod;
+import net.wdfeer.crazymod.item.custom.TitaniumSword;
 
 public class ModItems {
     public static final Item TITANIUM_INGOT = RegisterItem("titanium_ingot",
             new Item(new FabricItemSettings().group(ItemGroup.MATERIALS).fireproof()));
+    public static final Item TITANIUM_SWORD = RegisterItem("titanium_sword", new TitaniumSword());
     static Item RegisterItem(String name, Item item)
     {
         return Registry.register(Registry.ITEM, new Identifier(CrazyMod.MOD_ID, name), item);
