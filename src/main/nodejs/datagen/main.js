04b0d9fc1afec4e433351481d3bedb7149381c18
wdfeer <49950781+wdfeer@users.noreply.github.com>
2022-12-03 20:13:41 +0100

    Create Datagen for side-top blocks with node.js

---
+++ src/main/nodejs/datagen/main.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+function includesAll(array, required) {
+    for (let i = 0; i < required.length; i++) {
+        let element = required[i];
+        if (!array.includes(element)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function writeByTemplate(templatePath, dirPath, name) {
+    let template = fs.readFileSync(templatePath).toString();
+    let oldText = template;
+    let text = oldText.replace("<0>", name);
+    while (text != oldText) {
+        oldText = text;
+        text = oldText.replace("<0>", name);
+    }
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+    fs.writeFileSync(dirPath + name + '.json', text);
+}
+
+function writeSideTopBlock(generatedPath, name) {
+    writeByTemplate('./templates/models/sideTopBlockModel.json', generatedPath + 'assets/crazymod/models/block/', name);
+    writeByTemplate('./templates/models/blockItemModel.json', generatedPath + 'assets/crazymod/models/item/', name);
+    writeByTemplate('./templates/loot_tables/dropSelfBlockLootTable.json', generatedPath + 'data/crazymod/loot_tables/blocks/', name);
+    writeByTemplate('./templates/blockstates/genericBlockState.json', generatedPath + 'assets/crazymod/blockstates/', name);
+}
+
+function datagen(workspacePath) {
+    let data = fs.readFileSync('./sideTopBlocks.json');
+    let sideTopBlocks = JSON.parse(data).values;
+    sideTopBlocks.forEach(name => {
+        console.log("Generating Data for " + name);
+        writeSideTopBlock(workspacePath + '/generated/', name);
+    });
+}
+
+function tryDatagen(workspacePath) {
+    let files = fs.readdirSync(workspacePath);
+    if (includesAll(files, ['generated', 'resources'])) {
+        datagen(workspacePath);
+    } else {
+        tryDatagen(workspacePath + '/..');
+    }
+}
+
+tryDatagen('..');
